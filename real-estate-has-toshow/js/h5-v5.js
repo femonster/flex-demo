5e2f08105db7c52b8666f5c82df82f14df84acbf
renren <409465662@qq.com>
2018-04-13 11:17:26 +0800

    .

--- real-estate-has-toshow/js/h5-v5.js
+++
@@ -1,7 +0,0 @@
-var oMaskBox = document.querySelector(".mask-box");
-var oImgNums = document.querySelector(".img-nums");
-var oImgsBox = document.querySelector(".my-img-com");
-var oOtherImgsBoxs = document.querySelector(".other-img-com");
-var oSliderBox = document.querySelector(".slider-box");
-var oBack = document.querySelector(".i-back");
-var navBox = document.querySelector(".img-nav-box");
